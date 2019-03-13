31c102e94dfcebdec4fe5bf2d7573581109f04e5
youngho753 <45281293+youngho753@users.noreply.github.com>
2019-03-13 16:22:52 +0900

    Revert "Merge pull request #15 from jeonjuyeong/yh"
    This reverts commit 98ccce1b1e5ee85ca29beea21fc8f5e5255fe3ce, reversing
    changes made to 90cfe395ebb545a85c7e0b29ffe89e2d3e08d2c2.

--- src/main/java/com/movie/ctrl/MovieController.java
+++ src/main/java/com/movie/ctrl/MovieController.java
@@ -10,7 +10,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,12 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/movie/*")
 public class MovieController {
 	@GetMapping(value = "/getMovie",produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE })
-	public String movie(Model model) {
+	public void movie() {
 		String clientId = "qaIfvFuA2ML2UDeEWxaE";//애플리케이션 클라이언트 아이디값";
 		String clientSecret = "6MR8wmHlRk";//애플리케이션 클라이언트 시크릿값";
 		try {
 			String text = URLEncoder.encode("스파이더맨", "UTF-8");
-			String apiURL = "https://openapi.naver.com/v1/search/movie?query="+text; // json 결과
+			String apiURL = "https://openapi.naver.com/v1/search/movie?query="+ text; // json 결과
 			//String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // xml 결과
 			URL url = new URL(apiURL);
 			HttpURLConnection con = (HttpURLConnection)url.openConnection();
@@ -33,9 +32,9 @@ public class MovieController {
 			int responseCode = con.getResponseCode();
 			BufferedReader br;
 			if(responseCode==200) { // 정상 호출
-				br = new BufferedReader(new InputStreamReader(con.getInputStream(),"UTF-8"));
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
 			} else {  // 에러 발생
-				br = new BufferedReader(new InputStreamReader(con.getErrorStream(),"UTF-8"));
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
 			}
 			String inputLine;
 			StringBuffer resp = new StringBuffer();
@@ -51,23 +50,11 @@ public class MovieController {
 			 
 	        JSONParser jsonparser = new JSONParser();
 			JSONObject jsonObject = (JSONObject)jsonparser.parse(origin_str);
-			//JSONObject responseObject = (JSONObject)jsonObject.get("response");
+			JSONObject responseObject = (JSONObject)jsonObject.get("response");
 			
-			String title = (String) jsonObject.get("title");
-			String image = (String) jsonObject.get("image");
-			String subtitle = (String) jsonObject.get("subtitle");
-			String pubDate = (String) jsonObject.get("pubDate");
-			String director = (String) jsonObject.get("director");
-			String actor = (String) jsonObject.get("actor");
-			String userRating = (String) jsonObject.get("userRating");
-			
-			model.addAttribute("title",title);
-			model.addAttribute("image",image);
-			model.addAttribute("subtitle",subtitle);
-			model.addAttribute("pubDate",pubDate);
-			model.addAttribute("director",director);
-			model.addAttribute("actor",actor);
-			model.addAttribute("userRating",userRating);
+			String email = (String) responseObject.get("email");
+			String name = (String) responseObject.get("name");
+			String id = (String) responseObject.get("id");
 			
 			/*HttpSession session = request.getSession();
 			session.setAttribute("name", name);
@@ -81,9 +68,5 @@ public class MovieController {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		return "";
-	}
-	public void getMovieRank() {
-		
 	}
 }
