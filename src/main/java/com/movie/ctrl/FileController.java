f1c39739b65b49c99ca35b8341973b86d165d4e9
youngho753 <45281293+youngho753@users.noreply.github.com>
2019-03-22 09:34:43 +0900

    Update FileController.java

--- src/main/java/com/movie/ctrl/FileController.java
+++ src/main/java/com/movie/ctrl/FileController.java
@@ -26,7 +26,7 @@ public class FileController {
 			// 파일명을 받는다 - 일반 원본파일명
 			String oldName = request.getHeader("file-name");
 			// 파일 기본경로 _ 상세경로s
-			String filePath = "C:/teamproject190312/Movie/src/main/webapp/resources/upload/";
+			String filePath = "C:/Users/it/git/Movie/src/main/webapp/resources/upload/";
 			String saveName = sb.append(new SimpleDateFormat("yyyyMMddHHmmss")
 					.format(System.currentTimeMillis()))
 					.append(UUID.randomUUID().toString())
